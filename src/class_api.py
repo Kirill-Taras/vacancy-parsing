f1a3ea7ab863f0060189eb9002189e151e569464
kirill_tarasov <kirilltaras1792@yandex.ru>
2024-01-14 18:59:02 +0700

    Исправить класс для получения API с hh.ru

--- src/class_api.py
+++ src/class_api.py
@@ -6,14 +6,5 @@ class BasicAPI(ABC):
     """Абстрактный класс для получения API с сайтов."""
 
     @abstractmethod
-    def get_vacancies(self):
-        pass
-
-
-class SuperJobAPI(BasicAPI):
-    """Получение вакансий с платформы superjob.ru"""
-    def __init__(self):
-        pass
-
-    def get_vacancies(self):
+    def get_vacancies(self) -> list[dict]:
         pass
--- src/hh.py
+++ src/hh.py
@@ -4,49 +4,65 @@ from pprint import pprint
 
 import requests
 
+from config import HH_URL
 from src.class_api import BasicAPI
 
 
 class HeadHunterAPI(BasicAPI):
-    """Получение вакансий с платформы hh.ru"""
-    def __init__(self, url):
-        self.url = url
-
-    def get_vacancies(self):
-        keyword = input(f"Поиск по вакансиям: ")
-        headers = {"HH-User-Agent": "GetVacancies (my@email.com)"}
-        params = {
-            "text": keyword,  # строка для поиска по вакансиям.
-            "area": 113,  # код региона.
+    """Класс для получения вакансий с платформы hh.ru"""
+    def __init__(self, query):
+        self.query = query
+        self.params = {
+            "text": self.query,  # слово для поиска по вакансиям.
             "per_page": 100,  # количество вакансий.
-            "page": None  # номер страницы.
+            "page": None,  # номер страницы.
+            "search_field": "name"  # поле для поиска.
                   }
-        response = requests.get(self.url, headers=headers, params=params)
-        if response.ok:
-            file_name = f"{params['text']}_hh.json"
-            file_path = Path.joinpath(Path(__file__).parent.parent, "data", file_name)
-            data_response = response.json()
-            vacancies_info = list()
-            try:
-                for vacancy in data_response["items"]:
-                    info = {
-                        "name": vacancy.get("name"),
-                        "location": vacancy["area"].get("name"),
-                        "url": vacancy.get("alternate_url"),
-                        "employer": vacancy["employer"].get("name"),
-                        "salary_from": vacancy["salary"].get("from") if vacancy["salary"] else None,
-                        "salary_to": vacancy["salary"].get("to") if vacancy["salary"] else None,
-                        "description": vacancy.get("snippet").get("requirement")
-                    }
-                    vacancies_info.append(info)
-                with open(file_path, "w", encoding="utf-8") as file_json:
-                    json.dump(vacancies_info, file_json, indent=4, ensure_ascii=False)
-                return vacancies_info
-            except (ValueError, KeyError):
-                print("Вакансий под ваш запрос не существует")
-        else:
-            print("Запрос не выполнен")
+        self.headers = {"HH-User-Agent": "GetVacancies (my@email.com)"}
+
+    def get_vacancies(self) -> list[dict]:
+        """
+        Метод получения вакансий.
+        :return: Список с вакансиями
+        """
+        return requests.get(url=HH_URL, headers=self.headers, params=self.params).json()["items"]
 
 
-hh = HeadHunterAPI("https://api.hh.ru/vacancies")
-pprint(hh.get_vacancies())
+# hh = HeadHunterAPI("python")
+#
+# pprint(hh.get_vacancies())
+
+    # def get_vacancies(self):
+    #     keyword = input(f"Поиск по вакансиям: ")
+    #     headers = {"HH-User-Agent": "GetVacancies (my@email.com)"}
+    #     params = {
+    #         "text": keyword,  # строка для поиска по вакансиям.
+    #         "area": 113,  # код региона.
+    #         "per_page": 100,  # количество вакансий.
+    #         "page": None  # номер страницы.
+    #               }
+    #     response = requests.get(self.url, headers=headers, params=params)
+    #     if response.ok:
+    #         file_name = f"{params['text']}_hh.json"
+    #         file_path = Path.joinpath(Path(__file__).parent.parent, "data", file_name)
+    #         data_response = response.json()
+    #         vacancies_info = list()
+    #         try:
+    #             for vacancy in data_response["items"]:
+    #                 info = {
+    #                     "name": vacancy.get("name"),
+    #                     "location": vacancy["area"].get("name"),
+    #                     "url": vacancy.get("alternate_url"),
+    #                     "employer": vacancy["employer"].get("name"),
+    #                     "salary_from": vacancy["salary"].get("from") if vacancy["salary"] else None,
+    #                     "salary_to": vacancy["salary"].get("to") if vacancy["salary"] else None,
+    #                     "description": vacancy.get("snippet").get("requirement")
+    #                 }
+    #                 vacancies_info.append(info)
+    #             with open(file_path, "w", encoding="utf-8") as file_json:
+    #                 json.dump(vacancies_info, file_json, indent=4, ensure_ascii=False)
+    #             return vacancies_info
+    #         except (ValueError, KeyError):
+    #             print("Вакансий под ваш запрос не существует")
+    #     else:
+    #         print("Запрос не выполнен")
