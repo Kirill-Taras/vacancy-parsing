cd246dc8f26d6e838e155df87f970fbdbf84e0ba
kirill_tarasov <kirilltaras1792@yandex.ru>
2024-01-24 19:33:38 +0700

    Создать классы для работы с вакансиями на платформе hh и superjob.

--- src/jobs.py
+++ src/jobs.py
@@ -19,20 +19,44 @@ class Vacancy:
 
     @staticmethod
     def validate_salary(salary):
+        """Метод преобразования зарплаты"""
         if salary is None:
             return 0
         else:
             return salary
 
+    def average_salary(self):
+        """Вычислнение средней зарплаты"""
+        return (self.salary_to + self.salary_from) / 2
+
+    def __gt__(self, other):
+        """Метод сравнения зарплаты"""
+        return self > other
+
+    def __lt__(self, other):
+        """Метод сравнения зарплаты"""
+        return self < other
+
+    def __eq__(self, other):
+        """Метод сравнения зарплаты"""
+        return self == other
+
+    def __str__(self):
+        return (f"vacancy_name:{self.vacancy_name}, "
+                f"salary_from: {self.salary_from}, "
+                f"salary_to: {self.salary_to}, "
+                f"url_vacancy: {self.url_vacancy}, "
+                f"town_job: {self.town_job}, "
+                f"description_vacancy: {self.description_vacancy}")
+
 
 class HeadHunterVacancy(Vacancy):
-    pass
+
+    def __str__(self):
+        return f"HeadHunter {super().__str__()}"
 
 
 class SuperJobVacancy(Vacancy):
-    pass
 
-
-
-
-
+    def __str__(self):
+        return f"SuperJob {super().__str__()}"
